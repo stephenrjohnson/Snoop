0ee9f279f513c1507726a619df0c4a2da9a1d60d
Stephen <steve@thatbytes.co.uk>
2012-02-19 16:46:22 +0000

    refactors tests

--- spec/plugins/help_spec.rb
+++ spec/plugins/help_spec.rb
@@ -2,10 +2,5 @@ require "spec_helper"
 require "plugins/help"
 
 describe Help do
-  describe "listall" do
-    it "should return a youtube result" do
-		m = mock.as_null_object
-		Help.listall(m).should =~ /^.* : http:\/\/www.youtube.com\/.*$/
-    end
-  end
+
 end
