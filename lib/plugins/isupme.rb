4463588f76cedb822ce81a2e9773fe8fe7ad4f6c
Stephen Johnson <steve@thatbytes.co.uk>
2012-02-27 13:06:42 +0000

    add is it up

---
+++ lib/plugins/isupme.rb
@@ -0,0 +1,25 @@
+require 'cinch'
+require 'open-uri'
+
+class IsUp < CinchPlugin
+  include Cinch::Plugin 
+  set :help, "!is <site> up|down - Check with www.isup.me"
+
+  match /is (.*?) (up|down)(\?)?/, method: :runcheck
+
+  def check(domain)
+    url = "http://www.isup.me/#{domain}"
+    responce = open(url).read
+    if responce.match("It's just you.")
+      return "Its up and working must be just you"
+    elsif responce.match("It's not just you!")
+      return "Its not up"
+    else
+      return "Check yourself i dont know"
+    end
+  end
+
+  def runcheck(m, domain)
+    m.reply(check(domain))
+  end
+end
