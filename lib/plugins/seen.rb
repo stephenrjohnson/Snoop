93a7f57d3b3191289d9dec3195bbbd8fb515ac03
Stephen <steve@thatbytes.co.uk>
2014-02-16 19:56:40 +0000

    cleanup formatting

--- lib/plugins/seen.rb
+++ lib/plugins/seen.rb
@@ -3,7 +3,7 @@ class Seen < CinchPlugin
   listen_to :channel,  method: :log
   match /seen (.+)/,  method: :search
   set :help, "!seen <nick> - Check when a nick was last seen"
-  
+
   def initialize(*args)
     super
     @users = {}
@@ -11,7 +11,7 @@ class Seen < CinchPlugin
 
   def log(m)
     @users[m.user.nick] = "#{Time.now.asctime}] #{m.user} was seen in #{m.channel} saying #{m.message}"
-  end 
+  end
 
   def search(m, nick)
     respond(m, message(m,nick))
@@ -23,14 +23,14 @@ class Seen < CinchPlugin
     elsif nick == m.user.nick
       message = "That's you!"
     elsif @users.key?(nick)
-     message = @users[nick].to_s
+      message = @users[nick].to_s
     else
       message =  "I haven't seen #{nick}"
     end
-    return message 
+    return message
   end
 
   def respond (m, message)
-     m.reply message
+    m.reply message
   end
 end
