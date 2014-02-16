8a29055a2df28686e00e0191256d0f40433bfb29
Stephen <steve@thatbytes.co.uk>
2014-02-16 19:53:37 +0000

    cleanup formatting

--- lib/threads/WebInterface.rb
+++ lib/threads/WebInterface.rb
@@ -5,23 +5,23 @@ class WebInterfaceThread < CinchThread
   end
 
   def start
-  	  @bot.debug "webserver starting"
-      Rack::Handler::WEBrick.run self, :Port => Settings.webinterface.port
+    @bot.debug "webserver starting"
+    Rack::Handler::WEBrick.run self, :Port => Settings.webinterface.port
   end
 
   def call(env)
-  	request = Rack::Request.new(env)
-  	params =  request.GET()
-  	if params['key'] == nil
-  		[200, {"Content-Type" => "text/html"},["Please provide get params key,message,channel"]]
-  	elsif params['key'] == nil || params['message'] == nil || params['message'] == nil 
-  		[500, {"Content-Type" => "text/html"},["Please provide get params key,message,channel",params.inspect]]
-  	elsif params['key'] == $config['webinterface']['key'].to_s 
-  		@bot.dispatch(:webmessage, nil , params['message'].to_s, params['channel'].to_s)
-   	 	[200, {"Content-Type" => "text/html"},[params.inspect]]
-   	else
-   	 	[500, {"Content-Type" => "text/html"},["Error"]]
-   	end
+    request = Rack::Request.new(env)
+    params =  request.GET()
+    if params['key'] == nil
+      [200, {"Content-Type" => "text/html"},["Please provide get params key,message,channel"]]
+    elsif params['key'] == nil || params['message'] == nil || params['message'] == nil 
+      [500, {"Content-Type" => "text/html"},["Please provide get params key,message,channel",params.inspect]]
+    elsif params['key'] == $config['webinterface']['key'].to_s 
+      @bot.dispatch(:webmessage, nil , params['message'].to_s, params['channel'].to_s)
+      [200, {"Content-Type" => "text/html"},[params.inspect]]
+    else
+      [500, {"Content-Type" => "text/html"},["Error"]]
+    end
   end
 
 end
