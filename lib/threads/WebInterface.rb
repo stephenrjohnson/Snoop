462b6f21de4e5f290afef8428bfc5738adaffa12
Stephen Johnson <srj@thermeon.com>
2014-02-25 14:40:37 +0000

    Fix webinterface to work with new version of cinch

--- lib/threads/WebInterface.rb
+++ lib/threads/WebInterface.rb
@@ -13,14 +13,22 @@ class WebInterfaceThread < CinchThread
     request = Rack::Request.new(env)
     params =  request.GET()
     if params['key'] == nil
-      [200, {"Content-Type" => "text/html"},["Please provide get params key,message,channel"]]
+      [500, {"Content-Type" => "text/json"},["Please provide key"]]
     elsif params['key'] == nil || params['message'] == nil || params['message'] == nil 
-      [500, {"Content-Type" => "text/html"},["Please provide get params key,message,channel",params.inspect]]
-    elsif params['key'] == $config['webinterface']['key'].to_s 
-      @bot.dispatch(:webmessage, nil , params['message'].to_s, params['channel'].to_s)
-      [200, {"Content-Type" => "text/html"},[params.inspect]]
+      [500, {"Content-Type" => "text/json"},["Please provide get params key,message,channel",params.inspect]]
+    elsif params['key'] == Settings.webinterface.key.to_s 
+      message=Rack::Utils.unescape(params['message']).to_s
+      tchannel=Rack::Utils.unescape(params['channel']).to_s
+       
+      @bot.channels.each{ | channel |
+        if channel.name == tchannel
+          channel.send(message)
+	end
+      }
+
+      [200, {"Content-Type" => "text/json"},[params.inspect]]
     else
-      [500, {"Content-Type" => "text/html"},["Error"]]
+      [500, {"Content-Type" => "text/json"},["Error"]]
     end
   end
 
