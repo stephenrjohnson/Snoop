cae40c8ce3fa4536c0f51c7930c62fdb4e2cfb1d
Stephen Johnson <srj@thermeon.com>
2014-09-19 16:16:23 +0000

    Add karma

---
+++ lib/plugins/karma.rb
@@ -0,0 +1,67 @@
+require 'cinch'
+
+class Karma < CinchPlugin
+  include Cinch::Plugin
+  match /(\w*)[+]{2}/, method: :increment
+  match /(\w*)[-]{2}/, method: :decrement
+  match /karma[ ]?(\w*)?/, method: :show_scores
+  set :help, "<username>++ or <username>-- or karma <username> or karma"
+
+  def initialize(*args)
+    super
+    @scores_file = "/tmp/karma.json"
+    if File.exist? @scores_file
+      File.open(@scores_file, "r") do |f|
+        @users = JSON.parse(f.read)
+        @users.default = 0
+      end
+    else
+      @users = Hash.new(0)
+    end
+  end
+
+  def increment(m, nick)
+    puts "In increment"
+    if nick == @bot.nick
+      m.reply "Increasing my karma would result in overflow."
+    elsif nick == m.user.nick
+      m.reply "Just keep patting yourself on the back there, sport."
+    else
+      update_user(nick) { |nick| @users[nick] += 1 }
+      show_scores(m, nick)
+    end
+  end
+
+  def decrement(m, nick)
+     puts "In deccrement"
+    if nick == @bot.nick
+      m.reply "I wouldn't do that if I were you..."
+    elsif nick == m.user.nick
+      m.reply "There are special rooms on this network for self-flagellation."
+    else
+      update_user(nick) { |nick| @users[nick] -= 1 }
+      show_scores(m, nick)
+    end
+  end
+
+  def show_scores(m, nick)
+     if nick != ""
+	 m.reply "#{ nick } has #{ @users[nick] } awesome points."
+     else
+      sorted_users = @users.sort_by { |k, v| v }
+      top_scores   = sorted_users.take(5)
+      top_scores.each { |nick, score| m.reply "#{ nick } has #{ @users[nick] } awesome points." }
+     end
+  end
+  
+  def update_user(nick)
+    yield(nick)
+    save
+  end
+
+  def save
+    File.open(@scores_file, "w") do |f|
+      f.write(@users.to_json)
+    end
+  end
+end
