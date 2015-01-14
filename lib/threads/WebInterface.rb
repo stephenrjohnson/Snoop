30b215f292b192cb555ecec3de8f585ef891ac19
Stephen Johnson <srj@thermeon.com>
2015-01-14 15:18:17 +0000

    Allow us to pass a colour to cinch when printing a message in the chatroom

--- lib/threads/WebInterface.rb
+++ lib/threads/WebInterface.rb
@@ -1,4 +1,6 @@
 require 'rack'
+require 'cgi'
+
 class WebInterfaceThread < CinchThread
   def initialize(bot)
     @bot = bot
@@ -6,7 +8,7 @@ class WebInterfaceThread < CinchThread
 
   def start
     @bot.debug "webserver starting"
-    Rack::Handler::WEBrick.run self, :Port => Settings.webinterface.port
+    Rack::Handler::WEBrick.run(self, {:Port => Settings.webinterface.port, :Host => Settings.webinterface.bindaddress})
   end
 
   def call(env)
@@ -14,16 +16,27 @@ class WebInterfaceThread < CinchThread
     params =  request.GET()
     if params['key'] == nil
       [500, {"Content-Type" => "text/json"},["Please provide key"]]
-    elsif params['key'] == nil || params['message'] == nil || params['message'] == nil 
+    elsif params['key'] == nil || params['message'] == nil || params['message'] == nil
       [500, {"Content-Type" => "text/json"},["Please provide get params key,message,channel",params.inspect]]
-    elsif params['key'] == Settings.webinterface.key.to_s 
-      message=Rack::Utils.unescape(params['message']).to_s
-      tchannel=Rack::Utils.unescape(params['channel']).to_s
-       
+    elsif params['key'] == Settings.webinterface.key.to_s
+
+      if params['colour'] != nil
+        if params['colour'] == 'blue' then colour = :blue end
+        if params['colour'] == 'red' then colour = :red end
+        if params['colour'] == 'green' then colour = :green end
+        if params['colour'] == 'black' then colour = :black end
+        if params['colour'] == 'yellow' then colour = :yellow end
+        if params['colour'] == 'orange' then colour = :orange end
+      else
+        colour = ''
+      end
+      message=CGI.unescape(params['message'])
+      tchannel=CGI.unescape(params['channel'])
+
       @bot.channels.each{ | channel |
         if channel.name == tchannel
-          channel.send(message)
-	end
+          channel.send(Cinch::Formatting.format(colour,message))
+        end
       }
 
       [200, {"Content-Type" => "text/json"},[params.inspect]]
